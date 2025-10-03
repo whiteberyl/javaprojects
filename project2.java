3a3fa10281ae881c8f044f6629f032a479085ab4
whiteberyl <47698411+whiteberyl@users.noreply.github.com>
2025-10-02 17:32:01 -0700

    Update project2.java

--- project2.java
+++ project2.java
@@ -19,76 +19,59 @@ int number = 0; //Make something like int num = 0 and like if num != 5 loop
    System.out.println("5) Exit"); // exits  System.exit(0); "goodbye"
  
  //INPUT 
-   System.out.println("Enter a number:");
-   if (!input.hasNextInt()) { //data type check
-      System.out.println("Error, invalid option, try again.");
-      continue;
-   }     
- number = input.nextInt();
- input.nextLine(); // input flushing
+ System.out.println("Enter a number:");
 
-   if (number < 1 || number > 5) { //if valid input checking
-      System.out.println("Error, invalid option, try again.");
-      System.out.println();
-      continue;
-   }  
-
+if (input.hasNextInt()) {
+    number = input.nextInt();   
+} else {
+    number = 0;                
+    input.next();               
+}
+if (number < 1 || number > 5) {
+    System.out.println("Error, invalid option, try again.\n");
+} else {
    
 //Calculation && Output
-
-//Square
-if (number == 1) {  
+if (number == 1) {  //square 
       for (int i = 0; i < 5; i++) {        // height = 5 rows
          for (int j = 0; j < 7; j++) {     // width = 7 columns
              System.out.print("* ");
-   }
+            }
       System.out.println();
+      }
    }
-   }
- //down 
- if (number == 2) {  
+ else if  (number == 2) {  // down tri
    for (int i = 0; i < 6; i++) {       //6x6
       for (int j = 0; j <= i; j++) {
          System.out.print("* ");
-   }
+            }
       System.out.println();
-   }
-   
-   }
-//Up triangle 6
-if (number == 3) {
-   for (int i = 5; i >= 0; i--) {      //6x6
-      for (int j = 0; j <= i; j++) {
-         System.out.print("* ");
-   }
-      System.out.println();
-   }
-   
-   }
-int rwidth = generator.nextInt(11); // keep rerolling
-int rheight = generator.nextInt(7);
-
-if (number == 4) {  
-   while (rwidth < 2); { 
-      rwidth = generator.nextInt(11); // 2-10
-   }
-   while (rheight == 0); {
-      rheight = generator.nextInt(7); // 1-6
-   }      
-      for (int i = 0; i < rheight; i++) {       
-         for (int j = 0; j < rwidth; j++) { 
-             System.out.print("* ");
-   }
+      }
+    }
+   else if (number == 3) { // up tri
+      for (int i = 5; i >= 0; i--) {      //6x6
+         for (int j = 0; j <= i; j++) {
+            System.out.print("* ");
+         }
          System.out.println();
+      }  
    }
+   else if (number == 4) {  //random
+      int rwidth = generator.nextInt(9) + 2; //+2 because hits 0-1
+      int rheight = generator.nextInt(6) + 1; // +1 because hits 0 
+         for (int i = 0; i < rheight; i++) {       
+            for (int j = 0; j < rwidth; j++) { 
+                System.out.print("* ");
+         }
+         System.out.println();
+      }
    }
-if (number == 5) { //exit
-   System.out.println("Goodbye.");
-   System.exit(0);     
-  }
-  System.out.println(); // skips line after loop
-  }
-  }
-  }
- 
-//Output
+   else if (number == 5) { //exit
+      System.out.println("Goodbye.");
+      System.exit(0);     
+     }
+     System.out.println(); //extra credit
+        }
+      }
+   }
+}
